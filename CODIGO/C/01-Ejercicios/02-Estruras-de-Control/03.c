b2e003f479aca7ad33001e1a266915529d078495
Oscar Letelier <oscar.letelier@outlook.com>
2024-07-25 22:25:05 -0400

    numeros par del 0 al 20

--- CODIGO/C/01-Ejercicios/02-Estruras-de-Control/03.c
+++ CODIGO/C/01-Ejercicios/02-Estruras-de-Control/03.c
@@ -1,3 +1,22 @@
 /*
     Crea un programa que imprima los numeros pares del 1 al 20 usando un bucle while
 */
+
+
+#include <stdio.h>
+
+int i = 0;
+
+int main() {
+    printf("Aqui solo se mostraran los numeros pares que se encuentran entre el 1 y el 20\n");
+
+    while (i <= 20)
+    {
+        if(i % 2 == 0){
+            printf("%i\n", i);
+        }
+        i++;
+    }
+
+    return 0;
+}
