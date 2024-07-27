2e4c06c466598b0f99e75d61a289dd6a4639eff6
Oscar Letelier <oscar.letelier@outlook.com>
2024-07-27 19:13:12 -0400

    Ejercicio 4, calculo de area de un rectangulo

--- CODIGO/C/01-Ejercicios/01-Introduccion-a-la-Programacion/04.c
+++ CODIGO/C/01-Ejercicios/01-Introduccion-a-la-Programacion/04.c
@@ -5,8 +5,15 @@
 
 #include <stdio.h>
 
-
+int base, altura;
+int result;
 
 int main() {
+    printf("Ingrese la base y altura para calcular al area del rectangulo deseado\n");
+    scanf("%d", &base);
+    scanf("%d", &altura);
 
+    result = base * altura;
+    
+    printf("el area del rectangulo es\n %d", result); 
 }
