bee4c1af7342d63e75f8a921a2a26260e72917eb
Oscar Letelier <oscar.letelier@outlook.com>
2024-07-29 21:48:03 -0400

    nuevos ejercicios

---
+++ CODIGO/C/01-Ejercicios/02-Estruras-de-Control/04.c
@@ -0,0 +1,13 @@
+/*Escribe un programa que encuentre el mayor
+de tres numeros*/
+
+#include <stdio.h>
+
+int numOne = 1;
+int numTwo = 2;
+int numTree = 3;
+
+int main () {
+    printf("El numero mayor de la siguiente lista. 1. = 1\n 2. = 2\n 3. =3\n");
+    
+}
---
+++ CODIGO/C/01-Ejercicios/02-Estruras-de-Control/05.c
@@ -0,0 +1,4 @@
+/*Escribe un programa que imprima la tabla
+de multiplicar de un numero dado por el usuario*/
+
+#include <stdio.h>
