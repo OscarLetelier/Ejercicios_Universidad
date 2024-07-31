80102e14dfd61dd53d291c81aaa2d9f3f7228e2e
Oscar Letelier <oscar.letelier@outlook.com>
2024-07-31 12:44:04 -0400

    Ejercicio resuelto de Tabla de multiplicar

--- CODIGO/C/01-Ejercicios/02-Estruras-de-Control/05.c
+++ CODIGO/C/01-Ejercicios/02-Estruras-de-Control/05.c
@@ -2,3 +2,19 @@
 de multiplicar de un numero dado por el usuario*/
 
 #include <stdio.h>
+
+int main() {
+    int numero, i;
+
+    // Pedir al usuario que introduzca el número
+    printf("Introduce un numero para ver su tabla de multiplicar: ");
+    scanf("%d", &numero);
+
+    // Imprimir la tabla de multiplicar
+    printf("Tabla de multiplicar del %d:\n", numero);
+    for (i = 1; i <= 10; ++i) {
+        printf("%d x %d = %d\n", numero, i, numero * i);
+    }
+
+    return 0;
+}
