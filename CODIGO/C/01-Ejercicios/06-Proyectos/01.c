787c5f000017244c65d9a0ce9bd1dd4f31f39743
Oscar Letelier <oscar.letelier@outlook.com>
2024-08-13 20:57:19 -0400

    L

--- CODIGO/C/01-Ejercicios/06-Proyectos/01.c
+++
@@ -1,4 +0,0 @@
-/*
-Desarrolla una calculadora basica que permita al 
-usuario sumar, restar, multiplicar y dividir dos numeros
-*/
--- CODIGO/C/01-Ejercicios/06-Proyectos/02.c
+++
@@ -1,5 +0,0 @@
-/*
-Escribe un programa que genere un numero aleatorio entre 1 y 100 y permita al usuario adivinarlo.
-El programa debe indicar si el numero ingresado es mayor o menor que el numero secreto hasta que el 
-usuario lo adivine
-*/
